ff4b83bc49fa1446322546697c143a8c06e7316d
elisad5791 <elisad5791@mail.ru>
2025-01-04 20:52:57 +0500

    first commit

---
+++ src/BracketsChecker.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Mybrackets;
+
+class BracketsChecker
+{
+    static public function ckeckString($str)
+    {
+        $pattern = '/^[\(\)\s]+$/';
+        $check = preg_match($pattern, $str);
+        if ($check === 0) {
+            throw new InvalidArgumentException('Недопустимый формат строки');
+        }
+
+        $stack = 0;
+        $symbols = str_split($str);
+        $result = false;
+        foreach ($symbols as $symbol) {
+            if ($symbol == '(') {
+                $result = true;
+                $stack++;
+            } elseif ($symbol == ')') {
+                if ($stack == 0) {
+                    $result = false;
+                    break;
+                }
+                $stack--;
+            }
+        }
+        if ($stack != 0) {
+            $result = false;
+        }
+
+        return $result;
+    }
+}
